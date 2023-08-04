f9d13afa6a71d5cb58e1e5cc40033af73ef441fa
nathanriva <109824298+nathanriva@users.noreply.github.com>
2023-08-04 16:04:25 -0300

    Pomodoro funcional con tiempo de estudio y tiempo de descanso, no se puede repetir el bucle

--- pomo_appv0.0.py
+++ pomo_appv0.0.py
@@ -1,24 +1,43 @@
 ### Esta va a ser la app mas basica, sin GUI, simplemente codigo, para aprender bien la logica de un reloj y su programacion##
-
 import time
 import os
-
-#study = input("Cuantos minutos queres estudiar?")
-#rest = input("Cuantos minutos  queres descansar?")
 #def pomodoro(study, rest):
-#    countdown()
 
 #____Con esta funcion convertimos los minutos ingresados por el usuario en segundos. (1/60)____#
 def pasar_a_minutos(num):
     return num * 60
 
+#def separar_en_minutos(mins=120):
+    while 1==1:
+        if mins > 60:
+            mins // 60
+        print(mins)
+
 #____Con esta funcion restamos el tiempo ingresado hasta llegar a 0. (1/60)____#
 def countdown(t):
     while t > 0:
-        print (t)
+        if t >= 60:
+            min = t // 60
+            seg = t - (60 * min)
+        else:
+            min = 0
+            seg = t
+        print (f"{min}:{seg}")
         #time.sleep(x) sirve para esperar x segundo/s entre vuelta y vuelta.
         time.sleep(1)
         t -= 1
+        os.system("cls")
     print("SE TERMINO EL TIEMPOOO")
 
-countdown(int(input("cuantos segundos?:")))
+study = int(input("Cuantos minutos queres estudiar?:"))
+rest = int(input("Cuantos minutos  queres descansar?:"))
+
+def POMODORO(study, rest):
+    t = pasar_a_minutos(study)
+    r = pasar_a_minutos(rest)
+    countdown(t)
+    countdown(r)
+    print("TERMINASTE DE ESTUDIAR")
+    os.system("clear||cls")
+
+POMODORO(study, rest)
