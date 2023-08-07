6d3fd157e40113882d5393fbe9d8b823a6ae66bf
nathanriva <109824298+nathanriva@users.noreply.github.com>
2023-08-07 19:58:57 -0300

    Se ordeno el codigo y los archivos y se agrego una alarma.

--- pomo_appv0.0/main.py
+++ pomo_appv0.0/main.py
@@ -3,13 +3,14 @@
 #____Importamos los modulos para la aplicacion____#
 import time
 import os
+from playsound import playsound
 
 #____Con esta funcion convertimos los minutos ingresados por el usuario en segundos. (1/60)____#
 def pasar_a_minutos(num):
     return num * 60
     
 #____Con esta funcion restamos el tiempo ingresado hasta llegar a 0. (1/60)____#
-def countdown(t):
+def countdown(t, frame):
     while t > 0:
         if t >= 60:
             min = t // 60
@@ -17,12 +18,11 @@ def countdown(t):
         else:
             min = 0
             seg = t
-        print (f"{min}:{seg}")
+        print (f"{frame}:{min}:{seg}")
         #time.sleep(x) sirve para esperar x segundo/s entre vuelta y vuelta.
         time.sleep(1)
         t -= 1
         os.system("cls")
-    print("SE TERMINO EL TIEMPOOO")
 
 #___Definimos las dos variables para la funcion principal (no me gusta q esten afuera pero bueno)___#
 study = int(input("Cuantos minutos queres estudiar?:"))
@@ -32,10 +32,13 @@ rest = int(input("Cuantos minutos  queres descansar?:"))
 def POMODORO(study, rest):
     t = pasar_a_minutos(study)
     r = pasar_a_minutos(rest)
-    countdown(t)
-    countdown(r)
+    os.system("cls")
+    countdown(t, frame=("estudia"))
+    os.system("cls")
+    countdown(r, frame=("descansa"))
     print("TERMINASTE DE ESTUDIAR")
-    os.system("clear||cls")
+    #____Con esta linea definimos la alarma que va a sonar cuando termine el tiempo____#
+    playsound(os.path.join(os.path.dirname(__file__), "./sound/rntn.mp3"))
 
 #___Llamamos a la funcion___#
 POMODORO(study, rest)
